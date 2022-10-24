877014ac90e6ebc75e1648e1ae1a36860555f351
Olufemi Olubukunmi <phemiwrites@gmail.com>
2022-10-24 09:28:34 +0100

    loading setup + bg-variable

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className='loading'>
+      <div className="loading-graphic">
+        
+      </div>
+    </div>
   )
 }
 

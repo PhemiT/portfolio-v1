86c56d09f6281cc9bef09b8acaed960896863806
Olufemi Olubukunmi <phemiwrites@gmail.com>
2023-04-27 12:43:38 +0100

    update

--- modules/Work.tsx
+++ modules/Work.tsx
@@ -22,17 +22,6 @@ const Work = () => {
                         </div>
                     </div>
                     <div className="work">
-                        <h3>RIGHTMUUVE<sup>{`{under development}`}</sup></h3>
-                        <div className="info">
-                            <p>Wordpress website built for a logistics service utilising automated bookings.</p>
-                            <div className="links">
-                                <a href="https://rightmuuve.com/" target="_blank" rel="noopener noreferrer">
-                                    <span><Icon icon={ICONS.live} color='#DDBEA8' size={15} />Live</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="work">
                         <h3>GPT3-OPENAI WEBSITE</h3>
                         <div className="info">
                             <p>Pixel perfect replication of OPENAI landing page from Figma File. Built with React.js and CSS</p>

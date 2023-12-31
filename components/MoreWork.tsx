3bfdb1e91f8029213065f1ab1ab05f6abd0727db
Bukunmi <phemiwrites@gmail.com>
2023-12-31 14:30:48 +0100

    Update MoreWork.tsx

--- components/MoreWork.tsx
+++ components/MoreWork.tsx
@@ -35,6 +35,14 @@ const MoreWork = () => {
           />
           <Divider />
           <ModalWork 
+          heading='boldo'
+          desc='landing page'
+          features='HTML, CSS, JS'
+          repoLink='https://github.com/PhemiT/boldo'
+          liveLink='https://boldo-drab.vercel.app/'
+          />
+          <Divider />
+          <ModalWork 
           heading='weather app'
           desc='Reactive UI'
           features='HTML, CSS, JS - Dynamic Data'
